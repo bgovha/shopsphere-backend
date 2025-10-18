0fc84bd94db4013557ef06fbff75f2748e97b6f1
Brenda <brendagovha1@gmail.com>
2025-10-18 08:16:22 +0200

    configured admin gui

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Admin interface for categories"""
+    list_display = ('id', 'name', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('created_at',)
+    ordering = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Admin interface for products"""
+    list_display = ('id', 'name', 'category', 'price', 'in_stock_quantity', 'date_created')
+    list_filter = ('category', 'date_created', 'in_stock_quantity')
+    search_fields = ('name', 'description')
+    list_editable = ('price', 'in_stock_quantity')  # Edit directly in list view
+    ordering = ('-date_created',)
+    readonly_fields = ('date_created', 'updated_at')
+    
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('name', 'description', 'category')
+        }),
+        ('Pricing & Stock', {
+            'fields': ('price', 'in_stock_quantity')
+        }),
+        ('Media', {
+            'fields': ('image',)
+        }),
+        ('Timestamps', {
+            'fields': ('date_created', 'updated_at'),
+            'classes': ('collapse',)  # Collapsible section
+        }),
+    )
+    
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Profile
+
+
+class ProfileInline(admin.StackedInline):
+    """Display profile inline with user"""
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+
+class UserAdmin(BaseUserAdmin):
+    """Extended user admin with profile"""
+    inlines = (ProfileInline,)
+    list_display = ('username', 'email', 'is_staff', 'is_active', 'date_joined')
+    list_filter = ('is_staff', 'is_active', 'date_joined')
+    search_fields = ('username', 'email')
+
+
+# Unregister default User admin and register our custom one
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
